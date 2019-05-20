91ac3dbcee0fb68e3a59b739e202e27bd03539e8
VICTOR <VICTOR@192.168.1.4>
2019-05-20 23:39:45 +0200

    Primeiro comit

---
+++ src/Repositorio/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+
+package Repositorio;
+
+public class PrimeiraClasse {
+    
+    public static void main(String [] x){
+        
+        System.out.println("Repositorio.PrimeiraClasse.main()");
+    }
+    
+}
